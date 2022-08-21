b2f8b0a20a81152f81d4b561762bce93ded659bb
picpoint <picpoint@yandex.ru>
2022-08-21 22:33:49 +0300

    work with layout homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,11 +6,23 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+    <link rel="stylesheet" href="public/assets/users/css/style.css">
 </head>
 <body>
 
 
+    <section class="hp">
+        <div class="hp__wrapper">
+            <div class="hp__header">
+                <div class="hp__headerwrp">
+                    <a href="#">Личный кабинет</a>
+                </div>
+            </div>
+            <div class="hp__content">
 
+            </div>
+        </div>
+    </section>
 
 
 </body>
